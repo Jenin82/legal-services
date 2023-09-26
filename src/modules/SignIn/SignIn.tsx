a3ea4ff3dad826cd6257cca51d5635fc6e09dc50
Jenin82 <jenin8282@gmail.com>
2023-09-26 13:24:25 +0530

    signin wip

---
+++ src/modules/SignIn/SignIn.tsx
@@ -0,0 +1,33 @@
+import style from "./SignIn.module.css";
+import logo from "../../assests/signin.svg";
+
+const SignIn = () => {
+    return (
+        <>
+            <div className={style.Wrapper}>
+                <h1>Sign In</h1>
+                <img src={logo} alt="" />
+                <form action="">
+                    <label htmlFor="">Email Address</label>
+                    <input type="text" placeholder="Enter Email" />
+                    <label htmlFor="">Password</label>
+                    <input type="text" placeholder="Enter Password" />
+                    <div>
+                        <div>
+                            <input
+                                type="checkbox"
+                                className={style.checkbox}
+                                name=""
+                                id=""
+                            />
+                            <p>Remember me</p>
+                        </div>
+						<p>Forgot Password?</p>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
+};
+
+export default SignIn;
