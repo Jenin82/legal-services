67f5f9bcba1c36837108c2958d697724e9b56fb0
Jenin82 <jenin8282@gmail.com>
2023-09-26 10:53:07 +0530

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa"; 
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        VitePWA({
+            manifest: {
+                name: "Your App Name",
+                short_name: "AppName",
+                description: "Your app description",
+                display: "standalone",
+                background_color: "#ffffff",
+                theme_color: "#000000",
+                icons: [
+                    {
+                        src: "/path/to/your/icon-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/path/to/your/icon-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+            workbox: {
+                swDest: "sw.js",
+            },
+        }),
+    ],
+});
