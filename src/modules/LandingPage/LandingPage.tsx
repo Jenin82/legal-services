42531bfab6f85ee1ee4af61777d760aafa1b562b
Jenin82 <jenin8282@gmail.com>
2023-09-26 13:04:08 +0530

    landing page done

--- src/modules/LandingPage/LandingPage.tsx
+++ src/modules/LandingPage/LandingPage.tsx
@@ -1,18 +1,23 @@
-import styles from './LandingPage.module.css';
+import styles from "./LandingPage.module.css";
+import logo from "../../assests/logo.png"
 
 const LandingPage = () => {
-    return <> 
-           <div className={styles.signInContainer}>
-      <div className={styles.backgroundImage}></div>
-      <div className={styles.signInContent}>
-        <h1 className={styles.signInHeading}>Sign In</h1>
-        <div className={styles.signInButtons}>
-          <button className={styles.redButton}>Sign In</button>
-          <button className={styles.whiteButton}>Cancel</button>
-        </div>
-      </div>
-    </div>
-    </>
+    return (
+        <>
+            <div className={styles.backgroundImage}></div>
+            <div className={styles.signInContainer}>
+                <div className={styles.heading}>
+					<img src={logo} alt="" />
+                    <h1>Legal</h1>
+                    <h1 className={styles.bridge}>Bridge</h1>
+                </div>
+                <div className={styles.signInButtons}>
+                    <button className={styles.redButton}>Sign In</button>
+                    <button className={styles.whiteButton}>Cancel</button>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default LandingPage;
