13411abccd2ee4bcce1ced7e578cf8ac9400748e
Jenin82 <jenin8282@gmail.com>
2023-09-26 14:22:41 +0530

    signup done

--- src/modules/LandingPage/LandingPage.tsx
+++ src/modules/LandingPage/LandingPage.tsx
@@ -1,10 +1,8 @@
 import styles from "./LandingPage.module.css";
-import logo from "../../assests/logo.png"
+import logo from "../../assests/logo.png";
 import { useNavigate } from "react-router-dom";
 
-
 const LandingPage = () => {
-
     const navigate = useNavigate();
 
     return (
@@ -12,19 +10,27 @@ const LandingPage = () => {
             <div className={styles.backgroundImage}></div>
             <div className={styles.signInContainer}>
                 <div className={styles.heading}>
-					<img src={logo} alt="" />
+                    <img src={logo} alt="" />
                     <h1>Legal</h1>
                     <h1 className={styles.bridge}>Bridge</h1>
                 </div>
                 <div className={styles.signInButtons}>
-                    <button className={styles.redButton} onClick={() =>{
-                        navigate(`/signin`);
-                    }
-                       
-                    }>Sign In</button>
-                    <button className={styles.whiteButton} onClick={() =>{
-                        navigate(`/signup`);
-                    } }>Sign Up</button>
+                    <button
+                        className={styles.redButton}
+                        onClick={() => {
+                            navigate(`/signin`);
+                        }}
+                    >
+                        Sign In
+                    </button>
+                    <button
+                        className={styles.whiteButton}
+                        onClick={() => {
+                            navigate(`/signup`);
+                        }}
+                    >
+                        Sign Up
+                    </button>
                 </div>
             </div>
         </>
