78306e5379cc44e7535d51dcf9ca66dac06b7212
Alan0602 <alankanil1234@gmail.com>
2023-09-26 12:13:22 +0530

    feat():Landing page created

--- src/modules/LandingPage/LandingPage.tsx
+++ src/modules/LandingPage/LandingPage.tsx
@@ -1,5 +1,18 @@
+import styles from './LandingPage.module.css';
+
 const LandingPage = () => {
-    return <div>LandingPage</div>;
+    return <> 
+           <div className={styles.signInContainer}>
+      <div className={styles.backgroundImage}></div>
+      <div className={styles.signInContent}>
+        <h1 className={styles.signInHeading}>Sign In</h1>
+        <div className={styles.signInButtons}>
+          <button className={styles.redButton}>Sign In</button>
+          <button className={styles.whiteButton}>Cancel</button>
+        </div>
+      </div>
+    </div>
+    </>
 };
 
 export default LandingPage;
