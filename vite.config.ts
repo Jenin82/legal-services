4950178461202ad6c3ecca0d18b11cb39d25fe3f
Jenin82 <jenin8282@gmail.com>
2023-09-26 11:05:26 +0530

    Ss

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa"; 
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+    manifest: {
+        name: "Your App Name",
+        short_name: "AppName",
+        description: "Your app description",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+            {
+                src: "/path/to/your/icon-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+            },
+            {
+                src: "/path/to/your/icon-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+            },
+        ],
+    },
+    workbox: {
+        swDest: "sw.js",
+    },
+};
 
 export default defineConfig({
-    plugins: [
-        react(),
-        VitePWA({
-            manifest: {
-                name: "Your App Name",
-                short_name: "AppName",
-                description: "Your app description",
-                display: "standalone",
-                background_color: "#ffffff",
-                theme_color: "#000000",
-                icons: [
-                    {
-                        src: "/path/to/your/icon-192x192.png",
-                        sizes: "192x192",
-                        type: "image/png",
-                    },
-                    {
-                        src: "/path/to/your/icon-512x512.png",
-                        sizes: "512x512",
-                        type: "image/png",
-                    },
-                ],
-            },
-            workbox: {
-                swDest: "sw.js",
-            },
-        }),
-    ],
+    base: "./",
+    plugins: [react(), VitePWA(manifestForPlugin)],
 });
